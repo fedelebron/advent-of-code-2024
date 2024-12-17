1d75453a39153087803239cf8fc3120875973740
Federico Lebrón <federico.lebron@gmail.com>
2024-12-17 09:26:20 +0000

    Cleaning up 17.

--- 17/b.hs
+++ 17/b.hs
@@ -14,7 +14,7 @@ import Control.Monad
 -}
 program = [2,4,1,1,7,5,0,3,1,4,4,4,5,5,3,0]
 iConst k = mkInteger k >>= mkInt2bv 64
-oneRound ((a, c1, c3, c4, c7), res) i = do
+oneRound (a, cs@[c1, c3, c4, c7], res) _ = do
   b <- a `mkBvand` c7
   b <- b `mkBvxor` c1
   bs <- mkBv2int b False
@@ -23,25 +23,23 @@ oneRound ((a, c1, c3, c4, c7), res) i = do
   b <- b `mkBvxor` c4
   b <- b `mkBvxor` c
   r <- b `mkBvand` c7
-  return ((a, c1, c3, c4, c7), res ++ [r])
+  return (a, cs, res ++ [r])
 
 allRounds a = do
-  [c1, c3, c4, c7] <- sequence (map iConst [1, 3, 4, 7])
-  outs <- snd <$> foldM oneRound ((a, c1, c3, c4, c7), []) [1 .. 16]
+  constants <- sequence (map iConst [1, 3, 4, 7])
+  (a', _, outs) <- foldM oneRound (a, constants, []) [1 .. 16]
   targets <- mapM (mkInteger >=> mkInt2bv 64) program
-  mkAnd =<< zipWithM mkEq outs targets
+  z0 <- iConst 0
+  mkAnd =<< zipWithM mkEq (z0:outs) (a':targets)
 
-problem :: Maybe Integer -> Z3 (Maybe Integer)
 problem lim = do
   a <- mkFreshBvVar "a" 64
   case lim of 
     Nothing -> return ()
     Just x -> do
       y <- iConst x
-      mm <- mkBvult a y
-      assert mm
-  res <- allRounds a
-  assert res
+      assert =<< mkBvult a y
+  assert =<< allRounds a
   fmap snd $ withModel $ \m -> fromJust <$> evalInt m a
 
 bestSol problem = go Nothing
